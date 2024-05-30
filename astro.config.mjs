37ed62fdc66d76d5677fb511a130d201f971fc62
Angello Nunez <angellonzgz@gmail.com>
2024-05-30 10:41:39 -0400

    Add resources to redirect to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://angellonunez.github.io',
-    base: 'angellonunezcom'
+    site: 'https://angellonunez.com',
 });
